167aa5efafff5109b2449275da85535ff9332564
Jeevan Reddy Pappu <jeevanreddy@Jeevans-MacBook-Air.local>
2026-03-10 18:52:50 -0700

    Made Changes CSS

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,11 @@ export default function Hero() {
         <h1>Hi, I'm <span>Jeevan</span></h1>
         <h2>Software Engineer & Competitive Programmer</h2>
         <p>
-          Experienced Software Engineer with 3 years in Oracle Documaker,
-          strong expertise in Java, scalable backend systems and cloud technologies.
+          Full Stack Java Developer with ~3 years of experience building scalable backend systems using Java, 
+          Spring Boot, and microservices. 
+          Experienced in developing REST APIs, integrating SQL/NoSQL databases, 
+          and building responsive web applications with React. 
+          Passionate about cloud-native development, distributed systems, and clean software architecture.
         </p>
         <a href="#projects" className="hero-btn">View My Work</a>
       </div>
