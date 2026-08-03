3fd058684a83922fc722986f86ab80097f7af437
Jeevan Reddy Pappu <jeevanreddy@Jeevans-MacBook-Air.local>
2026-08-03 10:18:26 -0700

    08032026-1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,17 +9,25 @@ export default function Hero() {
           Hi, I'm <span>Jeevan</span>
         </h1>
 
-        <h2>Java Full Stack Developer</h2>
+        //<h2>Java Full Stack Developer</h2>
 
         <p>
-          Software Engineer experienced in building enterprise and cloud-native
-          applications using Java, Spring Boot, Microservices, React.js,
-          PostgreSQL, Apache Kafka, and AWS.
-          <br /><br />
-          Passionate about designing scalable distributed systems,
-          high-performance REST APIs, event-driven architectures,
-          and secure software solutions that power real-world business applications.
-        </p>
+  Software Engineer experienced in building enterprise-grade and cloud-native
+  applications using Java, Spring Boot, Microservices, React.js, PostgreSQL,
+  Apache Kafka, AWS, and RESTful APIs.
+  <br /><br />
+  Passionate about designing scalable distributed systems, high-performance APIs,
+  event-driven architectures, secure software solutions, and backend services
+  that power real-world business applications.
+  <br /><br />
+  Currently expanding my expertise in <strong>ASP.NET Core</strong>, <strong>C#</strong>,
+  <strong>Entity Framework Core</strong>, and modern .NET application development,
+  while actively building projects with <strong>Large Language Models (LLMs)</strong>,
+  <strong>Retrieval-Augmented Generation (RAG)</strong>, and
+  <strong>Agentic AI</strong>. I enjoy developing intelligent applications that
+  combine AI with scalable backend systems to automate workflows, enhance user
+  experiences, and solve complex real-world problems.
+</p>
 
         <div className="hero-buttons">
           <a href="#projects" className="hero-btn">
